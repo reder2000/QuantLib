d1b7f286fb589f4b60b4979e8f1584f9e487687a
Richard Rouge <richard.rouge@outlook.com>
2021-01-22 19:33:25 +0100

    remove more actions

--- tools/check_all_headers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# execute this script from the root QuantLib directory
-
-find ./ql -name *.hpp \
-| grep -v '/all\.hpp$' | grep -v '/auto_link\.hpp$' | grep -v 'config' \
-| sort | ./tools/check_header.py
-
--- tools/check_copyrights.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-# Run this from the main QuantLib directory
-
-grep -r --include='*.[hc]pp' "Copyright (C)" * | ./tools/collect_copyrights.py > LICENSE.TXT
-
